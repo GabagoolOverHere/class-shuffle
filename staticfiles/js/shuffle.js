9c57ebd3cb9589456d2a331c6d2d02055fe733e5
GabagoolOverHere <vicmartindev@gmail.com>
2022-03-20 22:44:19 +0100

    static files

---
+++ staticfiles/js/shuffle.js
@@ -0,0 +1,38 @@
+function data() {
+    return {
+        pickConfigShow: true,
+        weekShow: false,
+        uploadShow: false,
+        mode: '',
+        days: {
+            'BOTH': {
+                'FULL':
+                    [
+                        {'Monday': {count: 0, weekday: 0}},
+                        {'Tuesday': {count: 0, weekday: 1}},
+                        {'Wednesday': {count: 0, weekday: 2}},
+                        {'Thursday': {count: 0, weekday: 3}},
+                        {'Friday': {count: 0, weekday: 4}}
+                    ]
+            },
+            'GROUPS': {
+                'A':
+                    [
+                        {'Monday': {count: 0, weekday: 0}},
+                        {'Tuesday': {count: 0, weekday: 1}},
+                        {'Wednesday': {count: 0, weekday: 2}},
+                        {'Thursday': {count: 0, weekday: 3}},
+                        {'Friday': {count: 0, weekday: 4}}
+                    ],
+                'B':
+                    [
+                        {'Monday': {count: 0, weekday: 0}},
+                        {'Tuesday': {count: 0, weekday: 1}},
+                        {'Wednesday': {count: 0, weekday: 2}},
+                        {'Thursday': {count: 0, weekday: 3}},
+                        {'Friday': {count: 0, weekday: 4}}
+                    ]
+            }
+        }
+    }
+}
