f7c019d5e457bd80b35cad5898ee3ec9474c9ec1
GabagoolOverHere <vicmartindev@gmail.com>
2022-03-20 18:40:33 +0100

    ca maaaarche

--- shuffle/shuffle_logic.py
+++ shuffle/shuffle_logic.py
@@ -1,24 +1,24 @@
 import csv
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from itertools import islice
 import random
 from jours_feries_france import JoursFeries
 from vacances_scolaires_france import SchoolHolidayDates
 
 
-def dispatch_slots(groups: dict, count: int) -> dict:
+def dispatch_slots(groups: dict, count_full: int = 0, count_a: int = 0, count_b : int = 0) -> dict:
     """
     use get_available_slots() depending of the configuration (2 groups, or full classroom)
     :param groups: A and B of the BOTH for the full classroom
     :param count: the number of students
     :return: dict[group]: [dates available according to holidays]
     """
-    if len(groups) == 1 and groups.get('BOTH'):
-        days = groups.get('BOTH')
-        groups['BOTH'] = get_available_slots(days, count)
+    if len(groups) == 1 and groups.get('FULL'):
+        days = groups.get('FULL')
+        groups['FULL'] = get_available_slots(days, count_full)
     elif all([len(groups) == 2, groups.get('A'), groups.get('B')]):
-        groups['A'] = get_available_slots(groups['A'], 6)
-        groups['B'] = get_available_slots(groups['B'], 7)
+        groups['A'] = get_available_slots(groups['A'], count_a)
+        groups['B'] = get_available_slots(groups['B'], count_b)
 
     return groups
 
@@ -45,24 +45,40 @@ def get_available_slots(slot_list: list, count) -> list:
     return slots
 
 
-def get_students_list(file, mode: str) -> dict:
+def get_students_list(*files, mode: str) -> dict:
     """
     extract the students in the csv file, shuffle them, and return them in a dict
     :param file: the csv file
     :param mode: full, or groups
     :return: dict contaning a shuffled list of students
     """
-    d = {'BOTH': []} if mode == 'full' else {'A': [], 'B': []}
-    students = []
-    doc = csv.reader(file, delimiter=';')
-    # islice allow slicing on iterators
-    for row in islice(doc, 1, None):
-        students.append(' '.join(x.strip('"') for x in row[0].split() if not x.isupper()))
+    if mode == 'BOTH':
+        d = {'FULL': []}
+        for file in files:
+            students = []
+            doc = csv.reader(file, delimiter=';')
+            # islice allow slicing on iterators
+            for row in islice(doc, 1, None):
+                students.append(' '.join(x.strip('"') for x in row[0].split() if not x.isupper()))
 
-    random.shuffle(students)
-    d['BOTH'].extend(students)
+            random.shuffle(students)
+            d['FULL'].extend(students)
 
-    return d
+        return d
+
+    elif mode == 'GROUPS':
+        d = {'A': [], 'B': []}
+        for file in files:
+            students = []
+            doc = csv.reader(file[1], delimiter=';')
+            # islice allow slicing on iterators
+            for row in islice(doc, 1, None):
+                students.append(' '.join(x.strip('"') for x in row[0].split() if not x.isupper()))
+
+            random.shuffle(students)
+            d[file[0]].extend(students)
+
+        return d
 
 
 def students_to_spots(groups: dict, slots: dict):
@@ -78,17 +94,18 @@ def students_to_spots(groups: dict, slots: dict):
                 if len(slot[2]) < slot[1]:
                     slot[2].append(student)
                     break
-
     final_res = []
-    if len(groups) > 1:
+    if len(slots) > 1:
         for a in slots['A']:
             for b in slots['B']:
                 if a[0] == b[0]:
                     a[1] += b[1]
                     a[2].extend(b[2])
-                    final_res.append(a)
+                    slots['B'].remove(b)
+        final_res.extend(slots['A'])
+        final_res.extend(slots['B'])
     else:
-        final_res = slots['BOTH']
+        final_res = slots['FULL']
 
     return sorted(final_res, key=lambda r: r[0])
 
@@ -113,7 +130,39 @@ def get_slots_full(groups, file):
     """
     handle all the functions so I have to call just this one to have a result
     """
-    student_dict = get_students_list(file, 'full')
-    slots = dispatch_slots(groups, len(student_dict['BOTH']))
+    student_dict = get_students_list(file, mode='BOTH')
+    slots = dispatch_slots(groups, count_full=len(student_dict['FULL']))
 
     return students_to_spots(student_dict, slots)
+
+
+def get_slots_groups(*files, groups):
+    student_dict = get_students_list(('A', files[0]), ('B', files[1]), mode='GROUPS')
+    slots = dispatch_slots(groups, count_a=len(student_dict['A']), count_b=len(student_dict['B']))
+
+    return students_to_spots(student_dict, slots)
+
+
+def format_post_request(schedule: dict, mode: str):
+    """
+    format the request received in the post request to be compatible with all the logic
+    :param schedule: a dict of days selected
+    :param mode: full class or two groups
+    :return: a dict adapted to the other function in the module
+    """
+    groups = {}
+    if mode == 'BOTH':
+        groups = {'FULL': []}
+    elif mode == 'GROUPS':
+        groups = {'A': [], 'B': []}
+
+    for group in schedule:
+        for day in schedule[group]:
+            d = day[list(day.keys())[0]]
+            if d['count'] > 0:
+                for count in range(d['count']):
+                    today = date.today()
+                    if today.weekday() >= d['weekday']: d['weekday'] += 7
+                    groups[group].append((today + timedelta(d['weekday'] - today.weekday())).strftime("%d/%m/%Y"))
+
+    return groups
--- shuffle/views.py
+++ shuffle/views.py
@@ -3,32 +3,40 @@ import codecs
 
 from django.http import HttpResponse
 from django.shortcuts import render
-from datetime import timedelta, date
-from shuffle.shuffle_logic import get_slots_full, format_result
+from shuffle.shuffle_logic import get_slots_full, get_slots_groups, format_result, format_post_request
 
 
 def shuffle(request):
     days = request.POST.get('days')
     mode = request.POST.get('mode')
-    file = request.FILES.get('file-upload')
 
-    res = ''
+    full = request.FILES.get('full-file-upload')
 
-    if all([days, mode, file, mode == 'full']):
-        groups = {'BOTH': []}
-        for day in json.loads(days)['week']:
-            d = day[list(day.keys())[0]]
-            if d['count'] > 0:
-                for count in range(d['count']):
-                    today = date.today()
-                    if today.weekday() >= d['weekday']: d['weekday'] += 7
-                    groups['BOTH'].append((today + timedelta(d['weekday'] - today.weekday())).strftime("%d/%m/%Y"))
+    a_group = request.FILES.get('a-file-upload')
+    b_group = request.FILES.get('b-file-upload')
 
-        res = format_result(get_slots_full(groups, codecs.iterdecode(file, 'utf-8')))
+    if all([days, mode, full, mode == 'BOTH']):
+        schedule = json.loads(days)['BOTH']
+        groups = format_post_request(schedule, mode)
 
-    #elif all([days, mode, file, mode == 'groups']):
+        res = format_result(get_slots_full(groups, codecs.iterdecode(full, 'utf-8')))
 
-    if res:
+        response = HttpResponse(res, content_type='application/text charset=utf-8')
+        response['Content-Disposition'] = f'attachment; filename="result.txt"'
+
+        return response
+
+    elif all([days, mode, a_group, b_group, mode == 'GROUPS']):
+        schedule = json.loads(days)['GROUPS']
+        groups = format_post_request(schedule, mode)
+
+        res = format_result(
+            get_slots_groups(
+                codecs.iterdecode(a_group, 'utf-8'),
+                codecs.iterdecode(b_group, 'utf-8'),
+                groups=groups
+        ))
+
         response = HttpResponse(res, content_type='application/text charset=utf-8')
         response['Content-Disposition'] = f'attachment; filename="result.txt"'
 
